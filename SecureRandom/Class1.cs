45a962ca6080210ca0edfda1ee1eeadc07632330
plum-bee <rafakanex1@gmail.com>
2024-03-18 15:37:20 +0100

    feat: Secure Random class

---
+++ SecureRandom/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureRandom
+{
+    public class RandomGenerator
+    {
+        readonly RNGCryptoServiceProvider _csp;
+
+        public RandomGenerator()
+        {
+            _csp = new RNGCryptoServiceProvider();
+        }
+
+        public int Next(int minValue, int maxValue)
+        {
+            if (minValue >= maxValue)
+            {
+                throw new ArgumentOutOfRangeException("minValue must be lower than maxValue");
+            }
+            else
+            {
+                byte[] byteArray = new byte[4];
+                _csp.GetBytes(byteArray);
+
+                int randomSeed = BitConverter.ToInt32(byteArray, 0);
+                Random random = new Random(randomSeed);
+
+                return random.Next(minValue, maxValue);
+            }
+        }
+    }
+}
