29b2343c718c94475a1be9f1a5c9a2ab017e9e87
Christian Cuesta <christianluminor@gmail.com>
2024-03-21 16:51:49 +0100

    Download button gets XML

--- SpaceshipApp/frmEncryptedSender.Designer.cs
+++ SpaceshipApp/frmEncryptedSender.Designer.cs
@@ -31,53 +31,85 @@ namespace SpaceshipApp
         {
             this.btnNext = new System.Windows.Forms.Button();
             this.pnlEncryptedContainer = new System.Windows.Forms.Panel();
-            this.btnSelectPubKeyFile = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnDownloadKeyFile = new System.Windows.Forms.Button();
             this.btnSend = new System.Windows.Forms.Button();
             this.lblValidationCode = new System.Windows.Forms.Label();
             this.txtValidationCode = new System.Windows.Forms.TextBox();
-            this.txtPubKeyPath = new System.Windows.Forms.TextBox();
             this.lblPubKeyPath = new System.Windows.Forms.Label();
             this.pnlEncryptedContainer.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnNext
             // 
-            this.btnNext.Location = new System.Drawing.Point(951, 511);
-            this.btnNext.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNext.Location = new System.Drawing.Point(1070, 639);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(100, 28);
+            this.btnNext.Size = new System.Drawing.Size(112, 35);
             this.btnNext.TabIndex = 3;
             this.btnNext.Text = "Next";
             this.btnNext.UseVisualStyleBackColor = true;
             // 
             // pnlEncryptedContainer
             // 
-            this.pnlEncryptedContainer.Controls.Add(this.btnSelectPubKeyFile);
+            this.pnlEncryptedContainer.Controls.Add(this.richTextBox1);
+            this.pnlEncryptedContainer.Controls.Add(this.progressBar1);
+            this.pnlEncryptedContainer.Controls.Add(this.button1);
+            this.pnlEncryptedContainer.Controls.Add(this.btnDownloadKeyFile);
             this.pnlEncryptedContainer.Controls.Add(this.btnSend);
             this.pnlEncryptedContainer.Controls.Add(this.lblValidationCode);
             this.pnlEncryptedContainer.Controls.Add(this.txtValidationCode);
-            this.pnlEncryptedContainer.Controls.Add(this.txtPubKeyPath);
             this.pnlEncryptedContainer.Controls.Add(this.lblPubKeyPath);
-            this.pnlEncryptedContainer.Location = new System.Drawing.Point(16, 15);
-            this.pnlEncryptedContainer.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlEncryptedContainer.Location = new System.Drawing.Point(18, 19);
+            this.pnlEncryptedContainer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pnlEncryptedContainer.Name = "pnlEncryptedContainer";
-            this.pnlEncryptedContainer.Size = new System.Drawing.Size(1035, 489);
+            this.pnlEncryptedContainer.Size = new System.Drawing.Size(1164, 611);
             this.pnlEncryptedContainer.TabIndex = 2;
             // 
-            // btnSelectPubKeyFile
+            // richTextBox1
             // 
-            this.btnSelectPubKeyFile.Location = new System.Drawing.Point(618, 84);
-            this.btnSelectPubKeyFile.Name = "btnSelectPubKeyFile";
-            this.btnSelectPubKeyFile.Size = new System.Drawing.Size(75, 23);
-            this.btnSelectPubKeyFile.TabIndex = 5;
-            this.btnSelectPubKeyFile.Text = "...";
-            this.btnSelectPubKeyFile.UseVisualStyleBackColor = true;
+            this.richTextBox1.Location = new System.Drawing.Point(86, 161);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(510, 96);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(86, 105);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(510, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(596, 379);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnDownloadKeyFile
+            // 
+            this.btnDownloadKeyFile.Location = new System.Drawing.Point(646, 99);
+            this.btnDownloadKeyFile.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDownloadKeyFile.Name = "btnDownloadKeyFile";
+            this.btnDownloadKeyFile.Size = new System.Drawing.Size(198, 37);
+            this.btnDownloadKeyFile.TabIndex = 5;
+            this.btnDownloadKeyFile.Text = "Download Key";
+            this.btnDownloadKeyFile.UseVisualStyleBackColor = true;
+            this.btnDownloadKeyFile.Click += new System.EventHandler(this.btnDownloadKeyFile_Click);
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(76, 386);
+            this.btnSend.Location = new System.Drawing.Point(86, 482);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.Size = new System.Drawing.Size(84, 29);
             this.btnSend.TabIndex = 4;
             this.btnSend.Text = "SEND";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -85,42 +117,37 @@ namespace SpaceshipApp
             // lblValidationCode
             // 
             this.lblValidationCode.AutoSize = true;
-            this.lblValidationCode.Location = new System.Drawing.Point(73, 160);
+            this.lblValidationCode.Location = new System.Drawing.Point(82, 291);
             this.lblValidationCode.Name = "lblValidationCode";
-            this.lblValidationCode.Size = new System.Drawing.Size(107, 17);
+            this.lblValidationCode.Size = new System.Drawing.Size(121, 20);
             this.lblValidationCode.TabIndex = 2;
             this.lblValidationCode.Text = "Validation Code";
             // 
             // txtValidationCode
             // 
-            this.txtValidationCode.Location = new System.Drawing.Point(76, 194);
+            this.txtValidationCode.Location = new System.Drawing.Point(86, 333);
+            this.txtValidationCode.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtValidationCode.Name = "txtValidationCode";
-            this.txtValidationCode.Size = new System.Drawing.Size(454, 22);
+            this.txtValidationCode.Size = new System.Drawing.Size(510, 26);
             this.txtValidationCode.TabIndex = 3;
-            // 
-            // txtPubKeyPath
-            // 
-            this.txtPubKeyPath.Location = new System.Drawing.Point(76, 85);
-            this.txtPubKeyPath.Name = "txtPubKeyPath";
-            this.txtPubKeyPath.Size = new System.Drawing.Size(454, 22);
-            this.txtPubKeyPath.TabIndex = 1;
             // 
             // lblPubKeyPath
             // 
             this.lblPubKeyPath.AutoSize = true;
-            this.lblPubKeyPath.Location = new System.Drawing.Point(73, 54);
+            this.lblPubKeyPath.Location = new System.Drawing.Point(82, 68);
             this.lblPubKeyPath.Name = "lblPubKeyPath";
-            this.lblPubKeyPath.Size = new System.Drawing.Size(128, 17);
+            this.lblPubKeyPath.Size = new System.Drawing.Size(81, 20);
             this.lblPubKeyPath.TabIndex = 0;
-            this.lblPubKeyPath.Text = "Public Key file path";
+            this.lblPubKeyPath.Text = "Public Key";
             // 
             // frmEncryptedSender
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.pnlEncryptedContainer);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmEncryptedSender";
             this.Text = "Spaceship";
             this.pnlEncryptedContainer.ResumeLayout(false);
@@ -136,8 +163,10 @@ namespace SpaceshipApp
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.Label lblValidationCode;
         private System.Windows.Forms.TextBox txtValidationCode;
-        private System.Windows.Forms.TextBox txtPubKeyPath;
         private System.Windows.Forms.Label lblPubKeyPath;
-        private System.Windows.Forms.Button btnSelectPubKeyFile;
+        private System.Windows.Forms.Button btnDownloadKeyFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- SpaceshipApp/frmEncryptedSender.cs
+++ SpaceshipApp/frmEncryptedSender.cs
@@ -2,19 +2,57 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.IO;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DataAccessLibrary;
 
 namespace SpaceshipApp
 {
     public partial class frmEncryptedSender : Form
     {
+        private DatabaseConnection _databaseConnection;
         public frmEncryptedSender()
         {
             InitializeComponent();
+            _databaseConnection = new DatabaseConnection();
+        }
+
+        private void btnDownloadKeyFile_Click(object sender, EventArgs e)
+        {
+            GetKey();
+        }
+
+        private void GetKey()
+        {
+            try
+            {
+                DataSet dataSet = _databaseConnection.RetrieveAllDataFromTable("PlanetKeys");
+
+                if (dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0)
+                {
+                    string xmlPublicKey = dataSet.Tables[0].Rows[0]["XMLKey"].ToString();
+
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "XML files (*.xml)|*.xml";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        File.WriteAllText(saveFileDialog.FileName, xmlPublicKey);
+                        MessageBox.Show("XML public key downloaded successfully.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No XML public key found in the database.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error downloading XML public key: {ex.Message}");
+            }
         }
     }
 }
